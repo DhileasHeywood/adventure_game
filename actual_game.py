72c59cedbc84a24ebe14abd17082ad0262b7bec3
Dhileas <dhileas@localhost.localdomain>
2020-07-28 13:02:35 +0100

    some more classes defined

--- actual_game.py
+++ actual_game.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Place:
 
     def enter(self):
@@ -8,6 +10,7 @@ class Place:
 
 class Engine:
 
+    # place_map MUST be a Map object, or the play function won't work. DO NOT FORGET THIS.
     def __init__(self, place_map):
         self.place_map = place_map
 
@@ -15,37 +18,47 @@ class Engine:
     # This function is what runs the game. Every time you run this method, you 'play' the game.
     def play(self):
         # first we set where the current place is, by specifying it. We create an object of class Map, with the name of the opening place.
-        # We then use that object when making an object of class Engine. So the resulting Engine object will also have a dict of all the scenes
-        # dict of all the scenes, and will have a couple of methods, such as 'opening_place()' and 'next_place'
+        # We then use that object when making an object of class Engine. (composition and all)
+
+        # current_place will be equal to self.place_map with the opening_place() method applied. This will be written later.
+        # What it will do is run the place class with the key "place_map"
         current_place = self.place_map.opening_place()
+        # last_place will be the Finished() place class. What happens when you win!
         last_place = self.place_map.next_place("finished")
 
+        # Here we're setting up the game loop. This means that when you've not won, the game will keep running whichever room you're in.
         while current_place != last_place:
             next_place_name = current_place.enter()
-            current_scene = self.place_map.next_place(next_place_name)
+            current_place = self.place_map.next_place(next_place_name)
 
+        # This is just to make sure that the last scene runs, because we'll be out of the game loop if we get there.
         current_place.enter()
-
-#class Map:
-#
- #   def __init__(self, start_place):
-#        pass
-#
- #   def next_place(self, place_name):
- #       pass
-#
- #   def opening_place(self):
- #       pass
-
-a_map = Map("entry_room")
-a_game = Engine(a_map)
-a_game.play()
 
 
 class Death(Place):
 
+    quips = [
+        "You died. You suck at this!",
+        "You're such a loser",
+        "Git gud, scrub!",
+        "Even my kittens are better than this!",
+        "You're worse than Dhileas's jokes!",
+        "Fatality",
+        "WASTED",
+        "They say when you fall off your horse, you should get back on it... People are often wrong though.",
+        "If at first you don't succeed, try, try again.",
+        "Maybe next time.... Or the time after..?"
+    ]
+
     def enter(self):
-        pass
+        print(Death.quips[randint(0, len(self.quips)-1)])
+        print("Start again?")
+        answer = input("> ").lower()
+
+        if answer == "yes":
+            # I need to figure out how to restart the game, but I'll put it here.
+        else:
+            exit(1)
 
 class Entry_room(Place):
 
