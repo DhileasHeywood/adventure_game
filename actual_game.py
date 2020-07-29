ba39e85ebf1b1bec7a7a8abfc369d2da986a5b02
Dhileas <dhileas@localhost.localdomain>
2020-07-29 19:02:13 +0100

    first room working, and item query now works too

--- actual_game.py
+++ actual_game.py
@@ -1,13 +1,25 @@
 from random import randint
 from textwrap import dedent
 
-yes_answers = ["yes", "yup", "uhuh", "sure", "if i must", "definitely", "absolutely", "mhmm", "mmhmm", "yeah", "yeh", "ues", "ya", "ja", "si", "oui", "yar", "yah"]
+yes_answers = ["yes", "yup", "uhuh", "sure", "if i must", "definitely", "absolutely", "mhmm", "mmhmm", "yeah", "yeh",
+               "ues", "ya", "ja", "si", "oui", "yar", "yah", "yarp", "yes please", "sure thing", "yes pls"]
 no_answers = ["no", "nope", "nop", "nay", "nah", "no way"]
-joke_answers = ["kill myself"]
+joke_answers = ["kill myself", "poop", "summon key", "knock knock", "wingardium leviosa"]
+personal_answers = ["check items", "items", "what do i have", "what things am i carrying", "what are my things",
+                    "what am i carrying", "what stuff do i have", "what things do i have", "whats my stuff",
+                    "what weapons", "my weapons", "what weapons do i have", "what weapons have i got", "whats my weapons",
+                    "weapons", "where are my weapons", "which weapons do i have", "which weapons have i got",
+                    "what weapons am i carrying", "which weapons am i carrying", "do i have weapons", "do i have things",
+                    "do i have a sword", "check weapons", "gold", "how much gold do i have", "how much do i have",
+                    "how much gold am i carrying", "do i have money", "how much money do i have"]
+companion_answers = ["who am i travelling with", "who am i with", "who is with me", "i'm with who", "who with"]
+
 
 
 
 class Place:
+
+    _visited = False
 
     def enter(self):
         print("This place is not yet configured.")
@@ -74,11 +86,11 @@ class EntryRoom(Place):
 
     def __init__(self):
         self.door_locked = True
-        self.visited = False
 
     def enter(self):
-        if self.visited is not True:
-            self.visited = True
+        if self._visited is not True:
+            self._visited = True
+
             print(dedent("""
             You wake up in a room. You're all alone.
             The room is almost empty with just the chair you're sitting on, and a chest in the corner of the room.
@@ -89,16 +101,19 @@ class EntryRoom(Place):
             print(dedent("""
             You open the door and walk forward. 
             """))
+            self.door_locked = True
+            self._visited = False
 
-            return "finished"
+
+            return "tree_room"
 
         else:
             print(dedent("What do you do?"))
 
-        action = input("> ")
+        answer = input("> ")
 
-        if "door" in action:
-            if "key" in players_stuff.things:
+        if "door" in answer:
+            if "a key" in players_stuff.things:
                 print(dedent("""
                             You walk up to the door. It's locked, but there is a keyhole. You think it might fit the key from the chest. 
                             You put the key in the hole and turn. It fits. Do you want to unlock it?
@@ -109,8 +124,39 @@ class EntryRoom(Place):
                     self.door_locked = False
                     return "entry_room"
 
+                elif answer in joke_answers:
+                    print("Come on... Take this seriously!")
+                    return "entry_room"
+
+                elif answer in no_answers:
+                    print("You don't unlock the door. Do you want to stay here forever or something? Be Brave!")
+                    return "entry_room"
+
+                elif answer in personal_answers:
+                    players_stuff.query_all_things()
+                    return "entry_room"
+
+                elif answer in companion_answers:
+                    players_stuff.who_with()
+                    return "entry_room"
+
                 else:
                     return "entry_room"
+
+            elif answer in joke_answers:
+                print("Come on... Take this seriously!")
+                return "entry_room"
+
+            elif answer in no_answers:
+                return "entry_room"
+
+            elif answer in personal_answers:
+                players_stuff.query_all_things()
+                return "entry_room"
+
+            elif answer in companion_answers:
+                players_stuff.who_with()
+                return "entry_room"
 
             else:
                 print(dedent("""
@@ -120,7 +166,7 @@ class EntryRoom(Place):
                 # entry room. Since you've already visited the room, it'll ask what you want to do.
                 return "entry_room"
 
-        elif "chest" in action:
+        elif "chest" in answer:
             print(dedent("""
             You walk towards the chest. 
             You try to open it but the closing mechanism is rusty and stiff. It's proving tough to open. 
@@ -131,16 +177,48 @@ class EntryRoom(Place):
             answer = input("> ")
 
             if answer in yes_answers:
-                players_stuff.weapon = "Rusty Sword"
-                players_stuff.new_thing("key")
+                players_stuff.weapon = "a rusty sword"
+                players_stuff.new_thing("a key")
                 print(dedent("""
-                It might be rusty, but you feel safer knowing that you have some way to protect yourself. 
+                The sword may be rusty, but you feel safer knowing that you have some way to protect yourself. 
                 Who knows what might reside within these walls...
+                The key feels heavy in your pocket. What might it open?
                 """))
+                return "entry_room"
+
+            elif answer in joke_answers:
+                print("Come on... Take this seriously!")
+                return "entry_room"
+
+            elif answer in no_answers:
+                print("You don't unlock the door. Do you want to stay here forever or something? Be Brave!")
+                return "entry_room"
+
+            elif answer in personal_answers:
+                players_stuff.query_all_things()
+                return "entry_room"
+
+            elif answer in companion_answers:
+                players_stuff.who_with()
                 return "entry_room"
 
             else:
                 return "entry_room"
+
+        elif answer in joke_answers:
+            print("Come on... Take this seriously!")
+            return "entry_room"
+
+        elif answer in no_answers:
+            return "entry_room"
+
+        elif answer in personal_answers:
+            players_stuff.query_all_things()
+            return "entry_room"
+
+        elif answer in companion_answers:
+            players_stuff.who_with()
+            return "entry_room"
 
         else:
             print("Input not recognised")
@@ -186,7 +264,8 @@ class TableRoom(Place):
 class TreeRoom(Place):
 
     def enter(self):
-        pass
+
+
 
 
 class Finished(Place):
@@ -194,7 +273,6 @@ class Finished(Place):
     def enter(self):
         print("Congratulations! You've won!")
         return "finished"
-
 
 class Map:
 
@@ -207,7 +285,8 @@ class Map:
         "dragon_room": DragonRoom(),
         "corridor": Corridor(),
         "riddle_room": RiddleRoom(),
-        "finished": Finished()
+        "finished": Finished(),
+        "death": Death()
     }
 
     def __init__(self, start_place):
@@ -224,18 +303,15 @@ class Map:
 class Items:
     def __init__(self):
         self.gold = 0
-        self.weapon = None
-        self.companion = None
+        self.weapon = "no weapons"
+        self.companion = "nobody"
         self.things = ["clothes"]
-
-    def how_much_gold(self):
-        print(f"You have {self.gold} gold")
-
-    def what_weapon(self):
-        print(f"You have a {self.weapon}")
 
     def who_with(self):
         print(f"You're travelling with {self.companion}")
+
+    def query_all_things(self):
+        print(f"You're carrying {self.gold} gold, {self.weapon}, and you have", ', '.join(self.things[0:len(self.things)-1]), "and", f"{self.things[-1]}.")
 
     def add_gold(self, amount):
         self.gold = self.gold + amount
@@ -248,3 +324,5 @@ players_stuff = Items()
 a_map = Map("entry_room")
 a_game = Engine(a_map)
 a_game.play()
+
+players_stuff.query_all_things()
